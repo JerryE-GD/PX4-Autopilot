1dc50e02f531362efb5c481986af1050cb405a09
JerryE-GD <yaoshun.wang@connect.polyu.hk>
2026-01-07 20:07:05 +0800

    Update init.c

--- boards/geek/h743/src/init.c
+++ boards/geek/h743/src/init.c
@@ -40,6 +40,7 @@
 #include "board_config.h"
 #include "hw_config.h"  // 新增：引入Bootloader配置头文件（你已改好的hw_config.h）
 #include <stdint.h>     // 新增：兼容串口输出函数的类型定义
+#include "stm32h7xx.h"  // 仅新增这一行！解决RCC/USART1寄存器未定义
 #include <syslog.h>
 
 #include <nuttx/config.h>
